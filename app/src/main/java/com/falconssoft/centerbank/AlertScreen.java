9c329a14680fa344392059087a0a2fe278dee1ad
user54 <Rawan>
2020-06-20 17:51:51 +0300

    rawan (centerBank)

---
+++ app/src/main/java/com/falconssoft/centerbank/AlertScreen.java
@@ -0,0 +1,13 @@
+package com.falconssoft.centerbank;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlertScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in);
+    }
+}
---
+++ app/src/main/java/com/falconssoft/centerbank/EditerCheackActivity.java
@@ -0,0 +1,15 @@
+package com.falconssoft.centerbank;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditerCheackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editer_check_layout);
+    }
+
+}
