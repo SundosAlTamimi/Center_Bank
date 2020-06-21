5ac92c7b3037e728b43962d14980fac4cb87c92e
abeer <sundos.tamomo20@gmail.com>
2020-06-21 09:48:30 +0300

    abeer

---
+++ app/src/main/java/com/falconssoft/centerbank/AddCheque.java
@@ -0,0 +1,14 @@
+package com.falconssoft.centerbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCheque extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_cheque);
+    }
+}
