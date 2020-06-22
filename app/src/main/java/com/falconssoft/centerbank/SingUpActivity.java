893d27bc4ac1b05613505d728e82f580163ad43a
Tahani Ghanoum <tahanighanoum1234@gmail.com>
2020-06-22 16:41:31 +0300

    date picker

--- app/src/main/java/com/falconssoft/centerbank/SingUpActivity.java
+++ app/src/main/java/com/falconssoft/centerbank/SingUpActivity.java
@@ -1,14 +1,73 @@
 package com.falconssoft.centerbank;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SingUpActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
+public class SingUpActivity extends AppCompatActivity {
+    TextView date_text;
+    Date currentTimeAndDate ;
+    SimpleDateFormat df ;
+    String today ;
+    Calendar myCalendar;
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sing_up_layout);
+        init();
+        currentTimeAndDate = Calendar.getInstance().getTime();
+        df = new SimpleDateFormat("dd/MM/yyyy");
+        today = df.format(currentTimeAndDate);
+        date_text.setText(convertToEnglish(today));
+    }
+
+    private void init() {
+        date_text=(TextView)findViewById(R.id.Date);
+        myCalendar = Calendar.getInstance();
+        date_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(SingUpActivity.this, openDatePickerDialog(date_text), myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+    public String convertToEnglish(String value) {
+        String newValue = (((((((((((value + "").replaceAll("١", "1")).replaceAll("٢", "2")).replaceAll("٣", "3")).replaceAll("٤", "4")).replaceAll("٥", "5")).replaceAll("٦", "6")).replaceAll("٧", "7")).replaceAll("٨", "8")).replaceAll("٩", "9")).replaceAll("٠", "0").replaceAll("٫", "."));
+        return newValue;
+    }
+    public DatePickerDialog.OnDateSetListener openDatePickerDialog(final TextView editText) {
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel(editText);
+            }
+
+        };
+        return date;
+    }
+    private void updateLabel(TextView editText) {
+        String myFormat = "dd/MM/yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        editText.setText(sdf.format(myCalendar.getTime()));
+
     }
 }
