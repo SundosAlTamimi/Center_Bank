4a4a2d6ad7fac3e5bbef08e3d3efca29e1965814
abeer <sundos.tamomo20@gmail.com>
2020-06-24 14:02:07 +0300

    abeer ^^ add json

--- app/src/main/java/com/falconssoft/centerbank/EditerCheackActivity.java
+++ app/src/main/java/com/falconssoft/centerbank/EditerCheackActivity.java
@@ -2,15 +2,29 @@ package com.falconssoft.centerbank;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.method.MovementMethod;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -20,41 +34,56 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.core.content.ContextCompat;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+import static android.widget.LinearLayout.VERTICAL;
+
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-import cn.pedant.SweetAlert.SweetAlertDialog;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class EditerCheackActivity extends AppCompatActivity {
 
     LinearLayout linerEditing, linerBarcode;
-    TextView scanBarcode, AmouWord,date;
+    TextView scanBarcode, AmouWord, date;
     Button SingUpButton;
     EditText Danier, phails;
     private ProgressDialog progressDialog;
 
-    int flag=0;
+    int flag = 0;
     CircleImageView CheckPic;
     static final int CAMERA_PIC_REQUEST = 1337;
-    Date currentTimeAndDate ;
-    SimpleDateFormat df ;
-    String today ;
+    Date currentTimeAndDate;
+    SimpleDateFormat df;
+    String today;
     Calendar myCalendar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,17 +113,12 @@ public class EditerCheackActivity extends AppCompatActivity {
         CheckPic.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View v)
-            {
-                flag=0;
+            public void onClick(View v) {
+                flag = 0;
                 Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
             }
         });
-
-
-
-
 
 
     }
@@ -142,7 +166,6 @@ public class EditerCheackActivity extends AppCompatActivity {
         }
     };
 
-
     private void initi() {
         linerEditing = findViewById(R.id.linerEditing);
         linerBarcode = findViewById(R.id.linerBarcode);
@@ -153,8 +176,8 @@ public class EditerCheackActivity extends AppCompatActivity {
         SingUpButton = findViewById(R.id.SingUpButton);
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Please Waiting...");
-        CheckPic=findViewById(R.id.CheckPic);
-        date=findViewById(R.id.date);
+        CheckPic = findViewById(R.id.CheckPic);
+        date = findViewById(R.id.date);
         myCalendar = Calendar.getInstance();
 
         date.setOnClickListener(new View.OnClickListener() {
@@ -169,11 +192,10 @@ public class EditerCheackActivity extends AppCompatActivity {
 
     }
 
-
     //TextView itemCodeText, int swBarcode
     public void readBarCode() {
 
-        flag=1;
+        flag = 1;
 //        barCodTextTemp = itemCodeText;
         Log.e("barcode_099", "in");
         IntentIntegrator intentIntegrator = new IntentIntegrator(EditerCheackActivity.this);
@@ -184,8 +206,8 @@ public class EditerCheackActivity extends AppCompatActivity {
         intentIntegrator.setBarcodeImageEnabled(false);
         intentIntegrator.initiateScan();
 
-    }
 
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -204,13 +226,27 @@ public class EditerCheackActivity extends AppCompatActivity {
 //                barCodTextTemp.setText(Result.getContents() + "");
 //                openEditerCheck();
 
-                // TODO send data as json
+                    // TODO send data as json
 
-                showSweetDialog(true);
+                    showSweetDialog(true);
 
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data);
             }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
+
+        } else {//
+            try {
+                if (requestCode == CAMERA_PIC_REQUEST) {
+                    Bitmap image = (Bitmap) data.getExtras().get("data");
+                    if (image != null) {
+                        CheckPic.setImageBitmap(image);
+                    }
+                }
+            } catch (Exception e) {
+                Log.e("not getCamera", "message " + e.toString());
+            }
+
         }
     }
 
@@ -249,34 +285,17 @@ public class EditerCheackActivity extends AppCompatActivity {
             })
                     .show();
 
-                    linerEditing.setVisibility(View.VISIBLE);
-                    linerBarcode.setVisibility(View.GONE);
-                }
-
-            } else {
-                super.onActivityResult(requestCode, resultCode, data);
-            }
-        } else {//
-          try{
-            if (requestCode == CAMERA_PIC_REQUEST) {
-                Bitmap image = (Bitmap) data.getExtras().get("data");
-                if (image != null) {
-                    CheckPic.setImageBitmap(image);
-                }
-            }
-
-
-            }catch (Exception e){
-              Log.e("not getCamera","message "+e.toString());
-          }
-
+            linerEditing.setVisibility(View.VISIBLE);
+            linerBarcode.setVisibility(View.GONE);
         }
+
     }
 
     public String convertToEnglish(String value) {
         String newValue = (((((((((((value + "").replaceAll("١", "1")).replaceAll("٢", "2")).replaceAll("٣", "3")).replaceAll("٤", "4")).replaceAll("٥", "5")).replaceAll("٦", "6")).replaceAll("٧", "7")).replaceAll("٨", "8")).replaceAll("٩", "9")).replaceAll("٠", "0").replaceAll("٫", "."));
         return newValue;
     }
+
     public DatePickerDialog.OnDateSetListener openDatePickerDialog(final TextView editText) {
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -292,6 +311,7 @@ public class EditerCheackActivity extends AppCompatActivity {
         };
         return date;
     }
+
     private void updateLabel(TextView editText) {
         String myFormat = "dd/MM/yyyy"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
@@ -380,4 +400,4 @@ public class EditerCheackActivity extends AppCompatActivity {
     }
 
 
-    }
+}
