31ac15848f0e833572ff136c82ef7ad0b809229d
abeer <sundos.tamomo20@gmail.com>
2020-07-25 17:53:23 +0300

    abeer ^^   v37   test   signup =>   login =>   main =>

--- app/src/main/java/com/falconssoft/centerbank/CheckValidationResponse.java
+++ app/src/main/java/com/falconssoft/centerbank/CheckValidationResponse.java
@@ -1,12 +1,26 @@
 package com.falconssoft.centerbank;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.text.Html;
 import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 public class CheckValidationResponse {
+    Context context;
+
+    public CheckValidationResponse(Context context) {
+        this.context = context;
+    }
+
+    public CheckValidationResponse() {
+    }
 
     void showSnackbar(String text, boolean showImage, View coordinatorLayout) {
         Snackbar snackbar;
@@ -22,6 +36,57 @@ public class CheckValidationResponse {
             textViewSnackbar.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_error, 0, 0, 0);
         }
         snackbar.show();
+    }
+
+    public void showValidationDialog(boolean check, String customerName, String BankNo, String accountNo, String chequeNo) {
+        if (check) {
+            final Dialog dialog = new Dialog(context, R.style.Theme_Dialog);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setContentView(R.layout.dialog_after_validation);
+            dialog.setCancelable(false);
+
+            TextView bankNameTV = dialog.findViewById(R.id.dialog_validation_bankName);
+            TextView chequeWriterTV = dialog.findViewById(R.id.dialog_validation_chequeWriter);
+            TextView chequeNoTV = dialog.findViewById(R.id.dialog_validation_chequeNo);
+            TextView accountNoTV = dialog.findViewById(R.id.dialog_validation_accountNo);
+            TextView okTV = dialog.findViewById(R.id.dialog_validation_ok);
+            TextView cancelTV = dialog.findViewById(R.id.dialog_validation_cancel);
+
+            chequeWriterTV.setText(customerName);
+            accountNoTV.setText(accountNo);
+            chequeNoTV.setText(chequeNo);
+            okTV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+//                    checkLanguage();
+                    dialog.dismiss();
+                }
+            });
+
+            cancelTV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
+                }
+            });
+
+            dialog.show();
+            Window window = dialog.getWindow();
+            window.setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        } else {
+            new SweetAlertDialog(context, SweetAlertDialog.ERROR_TYPE)
+                    .setTitleText("WARNING")
+                    .setContentText("Invalidate cheque!")
+                    .setCancelText("Close").setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                @Override
+                public void onClick(SweetAlertDialog sweetAlertDialog) {
+                    sweetAlertDialog.dismissWithAnimation();
+
+                }
+            }).show();
+
+        }
+
     }
 
 }
